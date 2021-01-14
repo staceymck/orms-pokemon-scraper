8320e0e251f5dc1c8d7c81a97b4806836ee9727d
Stacey McKnight <mcknight.stm@gmail.com>
2021-01-13 18:28:23 -0600

    commit tree e5fce602abb9e212fb87c1921e4bb17acff40d19

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,31 @@
 class Pokemon
+  attr_accessor :name, :type, :id, :db
+
+  def initialize(name:, type:, db:, id: nil)
+    @name = name
+    @type = type
+    @db = db
+    @id = id
+  end
+
+  def self.save(name, type, db)
+      sql = <<-SQL
+        INSERT INTO pokemon (name, type)
+        VALUES (?, ?)
+      SQL
+      db.execute(sql, name, type)
+  end
+
+  def self.find(id, db)
+     sql = <<-SQL
+      SELECT * 
+      FROM pokemon
+      WHERE id = ?
+      LIMIT 1
+     SQL
+
+     db.execute(sql, id).map do |row|
+      self.new(name:row[1], type:row[2], db:db, id:row[0])
+     end.first
+  end
 end
